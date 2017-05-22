55059314522e5b8b7636a687f04177e713db82b6
csparx <csparks91@gmail.com>
2017-05-22 15:49:24 -0500

    Initial Theme File Creation

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function cns_enqueue_style() {
+  wp_enqueue_script('jquery');
+    if ( is_child_theme() ) {
+      // load parent stylesheet first if this is a child theme
+      wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'style.css', false );
+    }
+    // load active theme stylesheet in both cases
+    wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), false );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'cns_enqueue_style' );
+
+?>
---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+  <head>
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+
+  </head>
+  <body>
+    <header>
+      
+
+    </header>
