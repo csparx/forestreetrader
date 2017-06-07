0cc9e572585a6dffdfa0765b5a6c4935827a223a
csparx <csparks91@gmail.com>
2017-06-07 09:28:24 -0500

    Bootstrap Compatibility Added

--- header.php
+++ header.php
@@ -8,12 +8,19 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> >
   <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewpoint" content="width=device-width, intial-scale=1" />
     <title><?php wp_title(); ?></title>
+
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+
     <?php wp_head(); ?>
+
 
   </head>
   <body>
     <header>
-      
+      <div>
 
+      </div>
     </header>
